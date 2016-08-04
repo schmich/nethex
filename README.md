891266df69548bb2ce9f770a6fe8fbdfa6f8123a
Chris Schmich <schmch@gmail.com>
2016-08-04 18:43:45 -0500

    Run npm install if modules haven't been installed yet. Rename www to web.

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 - `git clone https://github.com/schmich/nethex`
 - `cd nethex/docker`
 - `docker build --tag schmich/nethex .`
-- `docker run -p 3000:3000 -it -v ~/dev/nethex:/srv/nethex/www schmich/nethex`
+- `docker run -p 3000:3000 -it -v ~/dev/nethex:/srv/nethex/web schmich/nethex`
 - `http://localhost:3000`
--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,9 @@
+if [ ! -d /srv/nethex/web/node_modules ]; then
+  cd /srv/nethex/web
+  npm install --no-bin-links
+fi
 /usr/bin/supervisord -c /etc/supervisord.conf &
-cd /srv/nethex/www && /usr/bin/pm2 start server.js --name nethex-web --watch /srv/nethex/www 2>&1 >/dev/null
+cd /srv/nethex/web && /usr/bin/pm2 start server.js --name nethex-web --watch /srv/nethex/web 2>&1 >/dev/null
 pm2 list 2>/dev/null
 supervisorctl status
 exec /bin/bash
